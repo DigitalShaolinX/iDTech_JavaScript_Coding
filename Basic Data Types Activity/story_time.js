c6c7655b8215204936f4d192b66204fbd20ac3b6
DigitalShaolinX <88642863+DigitalShaolinX@users.noreply.github.com>
2022-08-06 00:26:49 -0400

    Add files via upload

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,23 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let title = 'Survival Of The Fittest';
+let myName = 'bruh';
+let country = 'BruhBruhLand';
+let opening = "You, " + myName + " are lost in the wilderness of " + country;
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let height = "6 4"
+let timeStuckInTheWild = "9324983248"
+let year = "2022.012391049"
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let currentItems = ['knife', ' hand sanitizer', ' flashlight', ' bug spray', ' lighter'];
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let isAlive = true;
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(opening);
+console.log("Standing at " + height + " with " + timeStuckInTheWild + " years stuck here" + " in the year " + year);
+console.log("wWth the following items of a " + currentItems);
+console.log("Welcome to " + title + ", How will you survive!");
